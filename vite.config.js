0da5ce2c704ff0b9d78a5c29fc7056a2ecd31a76
hannescato <ditt@example.com>
2025-02-18 19:25:30 +0100

    base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hovet/", // 👈 Lägg till denna rad!
+  base: "/hovet/",
 })
